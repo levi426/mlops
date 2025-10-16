89a8e69755efd5dd5924d1259f0a77de996b6e44
unknown <l230626@fastlhr.nu.edu.pk>
2025-10-16 15:41:28 +0500

    Update home route for dashboard

--- ml/housepk_app.py
+++ ml/housepk_app.py
@@ -40,6 +40,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/", methods=["GET"])
 def index():
+    print("changes here")
     return render_template("index.html", feature_meta=feature_meta)
 @app.route('/login')
 def login():
