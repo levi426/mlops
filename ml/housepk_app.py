f294a6a9404575999dc808ba30a71e5abb26e233
levi426 <ahmadrouf88456@gmail.com>
2025-10-16 15:53:34 +0500

    this is login page api

--- ml/housepk_app.py
+++ ml/housepk_app.py
@@ -43,7 +43,7 @@ def index():
     return render_template("index.html", feature_meta=feature_meta)
 @app.route('/login')
 def login():
-    return "This is the login page"
+    return "This is the login "
 
 @app.route("/predict", methods=["POST"])
 def predict():
